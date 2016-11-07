81f0dc667901c15c9852cbb7db9488f7539b99fc
Thai Thien <tthien@apcs.vn>
2016-11-07 22:10:54 +0700

    update meow_rnn.lua

--- meow_rnn.lua
+++ meow_rnn.lua
@@ -8,11 +8,12 @@ function RNN.create(opt)
     local h_prev = nn.Identity()()
 
     local Wx = nn.Linear(opt.size,opt.size)({x})
-    local Uh = nn.Linear(opt.size,opt.size)({h_prev})
-    local sum = nn.CAddTable()({Wx,Uh})
+    local Uh_prev = nn.Linear(opt.size,opt.size)({h_prev})
+    local sum = nn.CAddTable()({Wx,Uh_prev})
     local h = nn.Tanh()({sum})
     
-    local y = nn.SoftMax()({nn.Linear(opt.size,opt.size)({h})})
+    local Vh = nn.Linear(opt.size,opt.size)({h})
+    local y = nn.LogSoftMax()({Vh})
 
     return nn.gModule({x,h_prev},{h,y})
 
